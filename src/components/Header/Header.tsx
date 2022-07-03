e2579a5f679b15ef0b456511c5aea6a32cbcaa6e
Carlos Sardina <developeralbertomt@gmail.com>
2022-07-03 01:42:29 -0500

    header

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,42 @@
+import styled from '@emotion/styled'
+import React from 'react'
+import { CometaTypography } from '../Shared'
+
+export interface HeaderProps {
+  title?: string,
+  logoUrl?: string,
+}
+
+const Container = styled('div')({
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+  borderBottom: '1px solid #e6e6e6',
+  backgroundColor: '#fff',
+  position: 'fixed',
+  width: '100%',
+  top: 0,
+  zIndex: 9999,
+})
+
+const ImgContainer = styled('div')({
+  width: '50px',
+  height: '50px',
+  marginRight: '1.25rem',
+})
+
+const StyledImage = styled('img')({
+  maxWidth: '100%',
+  maxHeight: '100%',
+})
+
+export const Header = ({ title, logoUrl }: HeaderProps) => {
+  return (
+    <Container>
+      <ImgContainer>
+        <StyledImage src={logoUrl} alt={title} />
+      </ImgContainer>
+      <CometaTypography>{title}</CometaTypography>
+    </Container>
+  )
+}
